996ef77dcd8077c65a651eb9b1ba483c1959dab1
German Lashevich <german.lashevich@gmail.com>
2017-11-19 00:15:31 +0100

    Initial hook version

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # tw-wifi-contexter
 Simple hook for taskwarrior which switches context according to WIFI SSID.
+Currently only MacOS is supported.
+
+The hook executes on each invocation of `task` command. If it can get WIFI SSID, that SSID is listed in config, and according context is not set, then the hook sets this context.
+If SSID is unknown or appropriate context already active, the hook does nothing. 
+
+
+## Usage
+
+All you need is to copy hook in hooks directory and create config.
+Config is ini-file with one section `[DEFAULT]`. Keys are contexts, values are lists of SSIDs.
+For example:
+```ini
+[DEFAULT]
+work=office, office_guest
+home=MY_WIFI_SPOT
+```
+
+```bash
+# Copy `on-launch.switch-context.py` to hooks directory (it's `~/.task/hooks` by default).
+cp src/on-launch.switch-context.py ~/.task/hooks/
+
+# Create ini config in hooks directory
+cp ssid_context_map.ini.example ~/.task/hooks/ssid_context_map.ini
+
+# Set mappings: context-to-SSIDs
+vim ~/.task/hooks/ssid_context_map.ini
+```
+
+## TODO
+
+- support for Windows and Linux
+- tests
+- on/off hook switcher in taskwarrior config
+- action for unknown SSID
---
+++ src/on-launch.switch-context.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+import configparser
+from subprocess import PIPE, run
+from sys import platform
+
+CONFIG_NAME = 'ssid_context_map.ini'
+
+
+def main():
+    ssid_context_map = read_config()
+    if ssid_context_map is None:
+        return
+    ssid = get_ssid()
+    if ssid is None:
+        return
+    if ssid in ssid_context_map:
+        context = ssid_context_map[ssid]
+        current_context = get_current_context()
+        if current_context != context:
+            set_context(context)
+            print('Set context "{0}", because of WiFi SSID "{1}"'.format(context, ssid))
+
+
+def get_task_command():
+    return ['task', 'rc.hooks=off']
+
+
+def get_current_context():
+    return run(get_task_command() + ['_get', 'rc.context'], stdout=PIPE).stdout.strip().decode('ascii') or 'none'
+
+
+def read_config():
+    config = configparser.ConfigParser()
+    config.read(CONFIG_NAME, encoding='utf-8')
+    data = config['DEFAULT']
+    ssid_context_map = {}
+    for context in data:
+        for ssid in data[context].split(','):
+            ssid = ssid.strip()
+            if ssid:
+                ssid_context_map[ssid] = context
+    return ssid_context_map
+
+
+def set_context(context):
+    run(get_task_command() + ['context', context])
+
+
+def get_ssid_macosx():
+    cmd = '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport'
+    for row in run([cmd, '-I'], stdout=PIPE, encoding='utf-8').stdout.split("\n"):
+        key, val = [x.strip() for x in row.split(':', 1)]
+        if key == 'SSID':
+            return val
+    return None
+
+
+def get_ssid_windows():
+    return None
+
+
+def get_ssid_linux():
+    return None
+
+
+def get_ssid():
+    if platform == 'darwin':
+        return get_ssid_macosx()
+    else:
+        return None
+        # if platform.startswith('linux'):
+        # elif platform.startswith('win32'):
+        # elif platform.startswith('cygwin'):
+        # elif platform.startswith('darwin'):
+        # elif platform.startswith('os2'):
+        # elif platform.startswith('os2emx'):
+        # elif platform.startswith('riscos'):
+        # elif platform.startswith('atheos'):
+
+
+if __name__ == '__main__':
+    main()
